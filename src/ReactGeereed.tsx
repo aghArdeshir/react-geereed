ccca2372df7f680d0cc29d23ff40e419c90f69a4
Ardeshir Izadi <ardeshireo@gmail.com>
2020-02-27 19:12:35 +0330

    initial use of useGeereedHook

--- src/ReactGeereed.tsx
+++ src/ReactGeereed.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { IReactGeereedProps, IGeereedColumn, IGeereedItem } from './typings';
+import { useGeereedSort, SORT_TYPES } from './hooks/use-geereed-hook';
 
 function ReactGeereed(props: IReactGeereedProps) {
   const { columns, items } = props;
+
+  const [sortKey, sortType, onSortCallback] = useGeereedSort();
+
   return (
     <table className="react-geereed">
       <thead>
         <tr>
-          {columns.map((column: IGeereedColumn) => <th key={column.key}>{column.title || column.key}</th>)}
+          {columns.map((column: IGeereedColumn) => (
+            <th
+              key={column.key}
+              style={{ color: sortKey === column.key ? 'red' : '' }}
+              onClick={() => onSortCallback(column.key)}
+            >
+              <div style={{ display: 'flex' }}>
+                {column.title || column.key}
+                <span style={{ marginLeft: 10, minWidth: 25 }}>
+                  {sortKey === column.key && sortType === SORT_TYPES.ASC ? <>&#9196;</> : <></>}
+                  {sortKey === column.key && sortType === SORT_TYPES.DES ? <>&#9195;</> : <></>}
+                </span>
+              </div>
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody>
