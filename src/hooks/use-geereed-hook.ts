3b65e10904fafde8f43cdcf19bab4a89955350a9
Ardeshir Izadi <ardeshireo@gmail.com>
2020-02-27 19:42:21 +0330

    fix,useGeereedSort: default ascending

--- src/hooks/use-geereed-hook.ts
+++ src/hooks/use-geereed-hook.ts
@@ -7,14 +7,14 @@ export enum SORT_TYPES {
 
 export function useGeereedSort(): [string, SORT_TYPES.ASC | SORT_TYPES.DES, (key: string) => void] {
     const [sortKey, setSortKey] = React.useState('');
-    const [sortType, setSortType] = React.useState(SORT_TYPES.DES);
+    const [sortType, setSortType] = React.useState(SORT_TYPES.ASC);
 
     const onSortCallback = React.useCallback((columnKey: string) => {
         if (sortKey === columnKey) {
-            if (sortType === SORT_TYPES.DES) { setSortType(SORT_TYPES.ASC); }
-            else if (sortType === SORT_TYPES.ASC) { setSortType(SORT_TYPES.DES); setSortKey(''); }
+            if (sortType === SORT_TYPES.ASC) { setSortType(SORT_TYPES.DES); }
+            else if (sortType === SORT_TYPES.DES) { setSortType(SORT_TYPES.ASC); setSortKey(''); }
         } else {
-            setSortType(SORT_TYPES.DES); setSortKey(columnKey);
+            setSortType(SORT_TYPES.ASC); setSortKey(columnKey);
         }
     }, [sortKey, sortType]);
 
