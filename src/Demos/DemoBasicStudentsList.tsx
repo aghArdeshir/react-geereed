574ee0acb9d1cd6a7a73a45cacd0e166d1d3e073
Ardeshir Izadi <ardeshireo@gmail.com>
2020-02-28 18:48:20 +0330

    minor code refactor

--- src/Demos/DemoBasicStudentsList.tsx
+++ src/Demos/DemoBasicStudentsList.tsx
@@ -2,27 +2,24 @@ import React from 'react';
 import { randomName, randomLastName, getRandom } from './Demo.Service';
 import ReactGeereed from '../ReactGeereed';
 
-export default function DemoBasicStudentsList() {
-  const columns = React.useMemo(
-    () => [
-      {
-        key: 'Name' // title will be set equal to key if not provided
-      },
-      { key: 'LastName', title: 'Last Name' },
-      { key: 'Age' },
-      { key: 'AverageMarks', title: 'Average Marks' }
-    ],
-    []
-  );
+const mockItems = new Array(10).fill(1).map(() => ({
+  Name: randomName(),
+  LastName: randomLastName(),
+  Age: getRandom(100),
+  AverageMarks: getRandom(20)
+}));
 
-  const [items, setItems] = React.useState(
-    new Array(10).fill(1).map(() => ({
-      Name: randomName(),
-      LastName: randomLastName(),
-      Age: getRandom(100),
-      AverageMarks: getRandom(20)
-    }))
-  );
+const columns = [
+  {
+    key: 'Name' // title will be set equal to key if not provided
+  },
+  { key: 'LastName', title: 'Last Name' },
+  { key: 'Age' },
+  { key: 'AverageMarks', title: 'Average Marks' }
+];
+
+export default function DemoBasicStudentsList() {
+  const [items, setItems] = React.useState(mockItems);
 
   const actions = React.useCallback(
     (rowItem, rowIndex: number) => {
