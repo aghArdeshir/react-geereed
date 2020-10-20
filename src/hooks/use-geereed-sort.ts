a246ff32e3e072645bf002379d65b3feae1cd039
aghArdeshir <ardeshireo@gmail.com>
2020-10-21 00:02:36 +0330

    add default sort key to and minor refactor

--- src/hooks/use-geereed-sort.ts
+++ src/hooks/use-geereed-sort.ts
@@ -1,26 +1,22 @@
 import React from 'react';
-import { IUseGeereedSort } from '../typings';
+import { IUseGeereedSort } from '../typings.d';
+import { GEEREED_SORT_TYPES } from '../utils/geereed-provider';
 
-export enum SORT_TYPES {
-  ASC = 'ASC',
-  DES = 'DES',
-}
-
-export function useGeereedSort(): IUseGeereedSort {
-  const [sortKey, setSortKey] = React.useState('');
-  const [sortType, setSortType] = React.useState(SORT_TYPES.ASC);
+export function useGeereedSort(defaultSortKey: string): IUseGeereedSort {
+  const [sortKey, setSortKey] = React.useState(defaultSortKey);
+  const [sortType, setSortType] = React.useState(GEEREED_SORT_TYPES.ASC);
 
   const onSortCallback = React.useCallback(
     (columnKey: string) => {
       if (sortKey === columnKey) {
-        if (sortType === SORT_TYPES.ASC) {
-          setSortType(SORT_TYPES.DES);
-        } else if (sortType === SORT_TYPES.DES) {
-          setSortType(SORT_TYPES.ASC);
+        if (sortType === GEEREED_SORT_TYPES.ASC) {
+          setSortType(GEEREED_SORT_TYPES.DES);
+        } else if (sortType === GEEREED_SORT_TYPES.DES) {
+          setSortType(GEEREED_SORT_TYPES.ASC);
           setSortKey('');
         }
       } else {
-        setSortType(SORT_TYPES.ASC);
+        setSortType(GEEREED_SORT_TYPES.ASC);
         setSortKey(columnKey);
       }
     },
