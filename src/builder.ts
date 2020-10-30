22000a47f6a2892adb8653fa9eb64d6dede059a4
aghArdeshir <ardeshireo@gmail.com>
2020-10-30 23:15:36 +0330

    bump version to 0.3.0 and use all hooks in builder

--- src/builder.ts
+++ src/builder.ts
@@ -1,3 +1,21 @@
+import { useGeereedColumns } from './hooks/use-geereed-columns';
+import { useGeereedDnd } from './hooks/use-geereed-dnd';
+import { useGeereedEditor } from './hooks/use-geereed-editor';
+import { useGeereedFilter } from './hooks/use-geereed-filter';
+import { useGeereedItems } from './hooks/use-geereed-items';
+import { useGeereedPagination } from './hooks/use-geereed-pagination';
 import { useGeereedSearch } from './hooks/use-geereed-search';
+import { useGeereedSelect } from './hooks/use-geereed-select';
+import { useGeereedSort } from './hooks/use-geereed-sort';
 
-export { useGeereedSearch };
+export {
+  useGeereedColumns,
+  useGeereedDnd,
+  useGeereedEditor,
+  useGeereedFilter,
+  useGeereedItems,
+  useGeereedPagination,
+  useGeereedSearch,
+  useGeereedSelect,
+  useGeereedSort,
+};
